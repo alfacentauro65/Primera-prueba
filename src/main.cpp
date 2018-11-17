f02fe510bb0399141c1f4ba2d4d8450dfabeef19
alfacentauro65 <alfacentauro65@gmail.com>
2018-11-17 12:04:27 +0100

    Formateando millis()

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,41 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 
-DHT dht(D3, DHT22);
+/*
+  Remapeo de pines del ESP8266
+*/
+const byte DP0 = 16;
+const byte DP1 = 5;
+const byte DP2 = 4;
+const byte DP3 = 0;
+const byte DP4 = 2; //BUILTIN_LED
+const byte DP5 = 14;
+const byte DP6 = 12;
+const byte DP7 = 13;
+const byte DP8 = 15;
 
-const int PIRPin = D4;
+/*
+  Conexión eléctrica ESP8266-HC4067
+  ESP8266 HC4067
+  ------- ------
+      DP8-S0 
+      DP7-S1 
+      DP6-S2 
+      DP5-S3 
+      A0 -SIG
 
+  *Nota.- No usar pines de entrada del multiplexor adyacentes, ya que 
+          se producen interacciones. Utilizar por ejemplo: 0, 3, 6, 9, 12, 15
+          para seis entradas analógicas.
+*/
 const int muxSIG = A0;
-const int muxS0 = D8;
-const int muxS1 = D7;
-const int muxS2 = D6;
-const int muxS3 = D5;
+const int muxS0 = DP8;
+const int muxS1 = DP7;
+const int muxS2 = DP6;
+const int muxS3 = DP5;
+
+DHT dht(DP3, DHT22);
+const int PIRPin = DP4;
 
 //Ajustes para MQ-135
 const int RL_VALUE = 20; // Resistencia RL del modulo en Kilo ohms
@@ -37,9 +63,9 @@ const float scope = (punto1[1] - punto0[1]) / (punto1[0] - punto0[0]);
 const float coord = punto0[1] - punto0[0] * scope;
 
 //MULTISENSOR1
-//const String URL = "/zipato-web/remoting/attribute/set?serial=ZTC3C2D55612AC3238&apiKey=bf159956-0bdf-4d19-bf22-78e0b164cfbc&ep=acdf3066-d9a3-49b0-938c-df52334ba6d7&";
+const String URL = "/zipato-web/remoting/attribute/set?serial=ZTC3C2D55612AC3238&apiKey=bf159956-0bdf-4d19-bf22-78e0b164cfbc&ep=acdf3066-d9a3-49b0-938c-df52334ba6d7&";
 //MULTISENSOR2
-const String URL = "/zipato-web/remoting/attribute/set?serial=ZTC3C2D55612AC3238&apiKey=b0a0d214-3167-4748-be7c-3eda0253cca0&ep=de9a94f3-8cbb-4d87-b4e4-a0243b57c61b&";
+//const String URL = "/zipato-web/remoting/attribute/set?serial=ZTC3C2D55612AC3238&apiKey=b0a0d214-3167-4748-be7c-3eda0253cca0&ep=de9a94f3-8cbb-4d87-b4e4-a0243b57c61b&";
 //MULTISENSOR3
 //const String URL = "/zipato-web/remoting/attribute/set?serial=ZTC3C2D55612AC3238&apiKey=abd05390-7703-4327-9edd-0cac4abb8d20&ep=1018dc46-cb88-412d-bcbb-ebdb8a6945cf";
 const char *host = "my.zipato.com";
@@ -49,8 +75,24 @@ const int httpsPort = 443;
 
 void EscribeLog(String txt)
 {
-  Serial.print(millis());
+
+  String sTime = "0000000000" + String(millis());
+  Serial.println(String(millis()));
+  Serial.println(sTime);
+  String sTimeFormated = "";
+  int iFrom = String(sTime).length();
+  int iTo = iFrom - 10;
+  for (int i = iFrom; i >= iTo; i--)
+  {
+    if (i == 2 || i == 5 || i == 8)
+    {
+      sTimeFormated = "." + sTimeFormated;
+    }
+    sTimeFormated = String(sTime).charAt(i) + sTimeFormated;
+  }
+  Serial.println(sTimeFormated);
   Serial.print(": -> ");
+  txt.replace("\n", "\n               ");
   Serial.println(txt);
 }
 
@@ -60,6 +102,7 @@ String strMejorWiFi()
 
   int maxRSSI = -100;
   String maxSSID = "";
+  unsigned long duration = millis();
   EscribeLog("-------->Entering into strMejorWiFi()");
 
   // Set WiFi to station mode and disconnect from an AP if it was previously connected
@@ -85,7 +128,7 @@ String strMejorWiFi()
     Serial.println("Networks found");
     for (int i = 0; i < n; ++i)
     {
-      if (WiFi.SSID(i).startsWith("HALO_2003") && WiFi.RSSI(i) > maxRSSI)
+      if (WiFi.SSID(i).startsWith(String(ssid)) && WiFi.RSSI(i) > maxRSSI)
       {
         maxSSID = WiFi.SSID(i);
         maxRSSI = WiFi.RSSI(i);
@@ -95,12 +138,13 @@ String strMejorWiFi()
     }
   }
   EscribeLog("Wifi elegida: " + maxSSID + ": " + maxRSSI);
-  EscribeLog("<--------Comminng out from strMejorWiFi()");
+  EscribeLog("<--------Comminng out from strMejorWiFi() in: " + String(millis() - duration) + " milisegundos");
   return (maxSSID);
 }
 
 void reconnect()
 {
+  unsigned long duration = millis();
   EscribeLog("-------->Entering into reconnect()");
 
   unsigned long timeOut = millis();
@@ -115,70 +159,76 @@ void reconnect()
   maxSSID.toCharArray(ssid, 50);
 
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED || millis() > timeOut + 30000)
+  while (WiFi.status() != WL_CONNECTED && millis() < timeOut + 30000)
   {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  EscribeLog("WiFi connected: " + maxSSID + ". IP Address: " + WiFi.localIP());
-  EscribeLog(maxSSID);
-   EscribeLog("<--------Comminng out from reconnect()");
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    EscribeLog("WiFi connected: " + maxSSID + ". IP Address: " + WiFi.localIP().toString());
+    EscribeLog(maxSSID);
+  }
+  else
+  {
+    EscribeLog("Error: WiFi not connected!");
+  }
+
+  EscribeLog("<--------Comminng out from reconnect() in: " + String(millis() - duration) + " milisegundos");
 }
 
 void getHttps(int atributo, double valor)
 {
   /* Hace la llamada HTTPS */
   // Use WiFiClientSecure class to create TLS connection
+  unsigned long duration = millis();
   EscribeLog("-------->Entering into getHttps()");
 
   WiFiClientSecure client;
   String myURL = URL + "value" + atributo + "=" + valor;
-  Serial.print("connecting to ");
-  Serial.println(host);
+  EscribeLog("Connecting to: " + String(host));
+
   if (!client.connect(host, httpsPort))
   {
-    Serial.println("connection failed");
+    EscribeLog("Connection failed!");
     return;
   }
 
-  Serial.print("requesting URL: ");
-  Serial.println(myURL);
+  EscribeLog((String("GET ") + myURL + " HTTP/1.1\r\n" +
+              "Host: " + host + "\r\n" +
+              "User-Agent: BuildFailureDetectorESP8266\r\n" +
+              "Connection: close\r\n\r\n"));
 
   client.print(String("GET ") + myURL + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
                "User-Agent: BuildFailureDetectorESP8266\r\n" +
                "Connection: close\r\n\r\n");
 
-  Serial.println("request sent");
+  EscribeLog("Request sent ...");
+  //Serial.println(client.readString());
+  String response = "";
   while (client.connected())
   {
-    String line = client.readStringUntil('\n');
-    if (line == "\r")
+    //MUY IMPORTANTE: Usar lo menos posible client.readString() Parece que repite la solicitud de request.
+    response = client.readString();
+    if (response.length() > 0)
     {
-      Serial.println("headers received");
+      EscribeLog("Headers received");
+      if (response.indexOf(" 200 OK") > 0)
+      {
+        EscribeLog(".....OK......");
+      }
+      else
+      {
+        EscribeLog("...FAILLURE...");
+      }
       break;
     }
   }
-  String line = client.readStringUntil('\n');
-  if (line.startsWith("{\"state\":\"success\""))
-  {
-    Serial.println("esp8266/Arduino CI successfull!");
-  }
-  else
-  {
-    Serial.println("esp8266/Arduino CI has failed");
-  }
-  Serial.println("reply was:");
-  Serial.println("==========");
-  Serial.println(line);
-  Serial.println("==========");
-  Serial.println("closing connection");
+
+  EscribeLog("Response: " + response);
   client.stop();
-  EscribeLog("<--------Comminng out from getHttps()");
+  EscribeLog("<--------Comminng out from getHttps() in: " + String(millis() - duration) + " milisegundos");
 }
 
 void SetMuxChannel(byte channel)
@@ -229,7 +279,7 @@ void loop()
   int presencia = digitalRead(PIRPin); //Devuelve HIGH o LOW
 
   //Lectura de calidad de aire
-  SetMuxChannel(13);
+  SetMuxChannel(0);
   //  float rs_med = readMQ(muxSIG);      // Obtener la Rs promedio
   //  float concentration = getConcentration(rs_med / R0); // Obtener la concentración
   long concentration = 0;
@@ -242,7 +292,7 @@ void loop()
   concentration = map(concentration, 0, 1023, 0, 9000);
 
   //Lectura de Luminosidad
-  SetMuxChannel(14);
+  SetMuxChannel(7);
   int luminosidad = analogRead(muxSIG);
 
   //Lectura de Potencia
@@ -253,21 +303,21 @@ void loop()
     potencia += abs(analogRead(muxSIG) - 512);
   }
   potencia /= 1000;
-  /*
+
   getHttps(1, temperatura);
   getHttps(2, humedad);
   getHttps(5, presencia);
   getHttps(6, luminosidad);
   getHttps(7, potencia);
   getHttps(8, concentration);
-*/
+  /*
   getHttps(1, 36.5 + random(1, 3));
   getHttps(2, 75.40 + random(1, 3));
   getHttps(5, 1);
   getHttps(6, 28 + random(1, 3));
   getHttps(7, 185 + random(1, 3));
   getHttps(8, 875 + random(1, 3));
-
+*/
   Serial.print("humedad: ");
   Serial.print(humedad);
   Serial.print(", temperatura: ");
@@ -288,11 +338,13 @@ void loop()
   Serial.print(", potencia: ");
   Serial.println(potencia);
 }
+
 // Obtener resistencia a partir de la lectura analogica
 float getMQResistance(int raw_adc)
 {
   return (((float)RL_VALUE / 1000.0 * (1023 - raw_adc) / raw_adc));
 }
+
 // Obtener la resistencia promedio en N muestras
 float readMQ(int mq_pin)
 {
